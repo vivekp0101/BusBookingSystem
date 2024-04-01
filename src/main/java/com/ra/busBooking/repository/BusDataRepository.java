8f167051a1b1abfee464784a7c5a9cb5ae6684ba
vivekp0101 <vp871693@gmail.com>
2024-04-01 21:42:10 +0530

    First Commit

---
+++ src/main/java/com/ra/busBooking/repository/BusDataRepository.java
@@ -0,0 +1,18 @@
+package com.ra.busBooking.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.ra.busBooking.model.BusData;
+
+@Repository
+public interface BusDataRepository extends JpaRepository<BusData, Integer>{
+	@Query(value = "select * from reservation where reservation.to_destination =:to and reservation.from_destination =:from and reservation.filter_date =:date  order By reservation.filter_date desc " , nativeQuery = true)
+	List<BusData> findByToFromAndDate(String to , String from, String date);
+	
+	
+
+}
